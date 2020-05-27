cb4d01221881e8e8eae2d3e030291da691b39474
annadom <65595104+annadom@users.noreply.github.com>
2020-05-27 10:18:14 +0100

    Update and rename r_steps.R to ist_steps.R

--- ist_steps.R
+++ ist_steps.R
@@ -12,14 +12,8 @@ library(ggplot2)
 ist <- read_csv("~/files/ist_corrected.csv", trim_ws = FALSE,
   col_types = cols(DRSUNKD = col_integer())) #DRSUNKD variable gives a parse error if not parsed manually
 
-#Edit colunm names, to work with SQL (avoid capital and dots)
-dbSafeNames = function(names){
-  names=gsub('[^a-z0-9]+','_',tolower(names))
-  names=make.names(names, unique = TRUE, allow_=TRUE)
-  names=gsub('.','_',names,fixed=TRUE)
-  names
-  }
-colnames(ist) = dbSafeNames(colnames(ist))
+#Edit colunm names, to lower case
+colnames(ist) <- tolower(colnames(ist))
 
 #In the pilot trial the medium heparin dose (M) is coded as H
 ist$rxhep <- revalue(ist$rxhep, c("H"="M"))
@@ -32,20 +26,20 @@ fact_design <- CrossTable(ist$rxasp, ist$rxhep, expected = FALSE, prop.r = TRUE,
 
 #Subset dataset for characteristics before randomisation
   #Variables used in the paper for baseline characteristics
-vars_char <- c("rdelay", "rconsc", "sex", "age", "rsleep", "ratrial", "rct", "rvisinf", "rhep_", "rasp_", "rsbp", "rdef__3", "stype")
+vars_char <- c("rdelay", "rconsc", "sex", "age", "rsleep", "ratrial", "rct", "rvisinf", "rhep24", "rasp3", "rsbp", "rdef3", "stype")
 ist_base_char  <- ist[vars_char]
   #Easier but larger dataset (SAME THING)
 vars_char <- ist[1:27]
 
 #Export to .csv to analyse with Workspace tools from .csv file
-write.csv(ist_base_char, "~/files/baseline_characteristics.csv", row.names = FALSE)
+write.csv(ist_base_char, "~/files/ist_base_char.csv", row.names = FALSE)
 #From Workspace Analyse:
   #Age box plot
-base_plot <- ggplot(dataset, aes(y = age))
-base_plot + geom_boxplot() + theme_classic()
+age_plot <- ggplot(ist_base_char, aes(y = age))
+age_plot + geom_boxplot() + theme_classic()
   #rdelay (delay between stroke and randomisation) box plot
-base_plot <- ggplot(dataset, aes(y = rdelay))
-base_plot + geom_boxplot() + theme_classic()
+rdelay_plot <- ggplot(ist_base_char, aes(y = rdelay))
+rdelay_plot + geom_boxplot() + theme_classic()
   #rsbp (systolic blood preassure at randomisation) density plot
-base_plot <- ggplot(dataset, aes(x = rsbp))
-base_plot + geom_density(alpha = 0.5) + theme_classic()
+rsbp_plot <- ggplot(ist_base_char, aes(x = rsbp))
+rsbp_plot + geom_density(alpha = 0.5) + theme_classic()
