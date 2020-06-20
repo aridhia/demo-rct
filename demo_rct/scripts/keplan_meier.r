4225c448be72e729242cf61b06c544be9a0488a3
annadom <65595104+annadom@users.noreply.github.com>
2020-06-20 16:56:46 +0100

    Create keplan_meier.r

---
+++ demo_rct/scripts/keplan_meier.r
@@ -0,0 +1,32 @@
+###############
+##KEPLAN MEIER#
+###############
+
+#Packages
+library(survival)
+library(survminer)
+library(readr)
+
+
+#Import data that will be used to do the analysis
+#Outcomes dataset, elaborated witht outcome.R 
+outcomes <- read_csv("~/files/results/outcomes.csv")
+
+#First, create a survival object that consist of the time and whether the endpoint was reached
+surv_object <- Surv(outcomes$time, outcomes$primary.endpoint)
+
+#fit the Kaplan-Meier Curve. In this case only use treatmentno
+fit <- survfit(surv_object ~ treatmentno, data = outcomes)
+
+
+#Graph the fit
+ggsurvplot(fit, data=outcomes, pval = TRUE,
+    risk.table = TRUE,
+    xscale = "d_y",
+    break.time.by = 365.25,
+    xlab = "Time since randomisation (years)",
+    ylab = "Without clinical recurrence(%)",
+    legend = "bottom",
+    legend.labs = c("Mercaptopurine", "Placebo"),
+    censor = FALSE,
+    tables.y.text = FALSE)
