711f19c5a40a239f57da8143e4201d2c1e91f8e4
annadom <65595104+annadom@users.noreply.github.com>
2020-08-10 09:39:52 +0100

    Update keplan_meier.r

--- demo_rct/scripts/keplan_meier.r
+++ demo_rct/scripts/keplan_meier.r
@@ -12,25 +12,53 @@ library(readr)
 #Outcomes dataset, elaborated witht outcome.R 
 outcomes <- read_csv("./demo_rct/results/outcomes.csv")
 
-#First, create a survival object that consist of the time and whether the endpoint was reached
-surv_object <- Surv(outcomes$time, outcomes$primary.endpoint)
+###############
+##KEPLAN MEIER#
+###############
 
-#fit the Kaplan-Meier Curve. In this case only use treatmentno
-fit <- survfit(surv_object ~ treatmentno, data = outcomes)
+#Packages
+library(survival)
+library(survminer)
+library(readr)
+
+
+#Import data that will be used to do the analysis
+#Outcomes dataset, elaborated witht outcome.R 
+outcomes <- read_csv("./results/outcomes.csv") 
+
+outcomes$treatmentno <- outcomes$treatmentno %>% 
+    # Treatmentno has to be converted into a factor variable
+    as.factor() %>% 
+    # By default the reference vale of treatmentno is Active (alphabetically determined). This has to be reversed
+    forcats::fct_rev()
+
+
+# Fit for the Kaplan-Meier Curve
+primary_fit <- survfit(Surv(outcomes$primary.time, outcomes$primary.endpoint) ~ treatmentno, data = outcomes)
+secondary_fit <- survfit(Surv(outcomes$secondary.time, outcomes$secondary.endpoint) ~ treatmentno, data = outcomes)
 
 
 #Graph the fit
-plot <- ggsurvplot(fit, data=outcomes, pval = TRUE,
-    risk.table = TRUE,
-    xscale = "d_y",
-    break.time.by = 365.25,
-    xlab = "Time since randomisation (years)",
-    ylab = "Without clinical recurrence(%)",
-    legend = "bottom",
-    legend.labs = c("Mercaptopurine", "Placebo"),
-    censor = FALSE,
-    tables.y.text = FALSE)
-#Show plot in Rstudio
-show(plot)
-#Save plot in the results folder
-ggsave("keplan_meier_plot.png", plot = show(plot), path = "./demo_rct/results/")
+
+keplan_meier <- function(fit) {
+    plot <- ggsurvplot(fit, data=outcomes, pval = TRUE,
+        risk.table = TRUE,
+        xscale = "d_y",
+        break.time.by = 365.25,
+        xlab = "Time since randomisation (years)",
+        ylab = "Without clinical recurrence(%)",
+        legend = "bottom",
+        legend.labs = c("Mercaptopurine", "Placebo"),
+        censor = FALSE,
+        tables.y.text = FALSE)
+
+    #Show plot
+    show(plot)
+}
+
+# Graph for primary endpoint
+keplan_meier(primary_fit)
+
+# Graph for secondary endpoint
+keplan_meier(secondary_fit)
+
