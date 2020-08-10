205e259c9e17aa4b9bf79f4453988302a74ac583
annadom <65595104+annadom@users.noreply.github.com>
2020-08-10 09:11:00 +0100

    Update outcome.r
    Change how to get censored times

--- demo_rct/scripts/outcome.r
+++ demo_rct/scripts/outcome.r
@@ -1,9 +1,9 @@
-#########
-#This code creates a dataset that allows the analysis of the primary outcome
-#########
+###############################################################################
+# This code creates a dataset that allows the analysis of the primary outcome #
+###############################################################################
 
 
-#PACKAGES
+# PACKAGES
 library(readr)
 library(dplyr)
 library(Hmisc)
@@ -11,96 +11,48 @@ library(Hmisc)
 
 #FUNCTIONS
 
-basic_edit <- function(dataset) {
-    #Column names to lowercase
-    colnames(dataset) <- tolower(colnames(dataset))
-    #Drop columns with removed information
-    rem <- sapply(colnames(dataset), function(i) any(dataset[[i]] == "Removed"))
-    dataset <- dataset[!rem]
-    return(dataset)
-}
+source("./demor_rct/scripts/functions.r")
 
-col_class <- function(dataset) {
     
-    dataset <- dataset %>%
-        #Change all columns that contain date/day in the column name to numeric
-        mutate_if(grepl('date|day', colnames(dataset)), as.numeric) %>%
-        #Change subject number to character
-        mutate(a_subjectno = as.character(a_subjectno))
-    return(dataset)
-}
-
-
-#Import baseline_factors generated with the file "baseline.r"
-baseline <- read_csv("./demo_rct/results/baseline_factors.csv") %>%
-    col_class()
-    
-#STATUS OF ARBITRATION
-#Dataset with information of whether each participant reached the endpoint (disease recurrence) or not and the time at whcih it happened
+# Dataset with information of whether each participant reached the endpoint (disease recurrence) or not and the time at whcih it happened
 status_arbitration <- read.csv("./demo_rct/trial_data/tbla_IMPStatusArbitration.csv") %>%
     basic_edit() %>%
-    ##Primary endpoint date is treated as a factor when reading csv, have to change it to character before applying the col_class function
-    mutate(studydayprimarydate = as.character(studydayprimarydate)) %>%
+    # Endpoint date is treated as a factor when reading csv, have to change it to character before applying the col_class function
+    mutate(studydayprimarydate = as.character(studydayprimarydate),
+           studydaysecondarydate = as.character(studydaysecondarydate)) %>%
     col_class() %>%
-    #Not interested in the secondary enpoint
-    subset(select = -c(secondary.endpoint, studydaysecondarydate)) %>%
-    #To do the Cox analysis, the endpoint has to be coded as 1/0
-    mutate(primary_endpoint = primary.endpoint) %>%
-    mutate(primary.endpoint = ifelse(primary.endpoint == "YES", 1,0))
+    # To do the Cox analysis, the endpoint has to be coded as 1/0
+    mutate(primary_endpoint = primary.endpoint, 
+           secondary_endpoint = secondary.endpoint,
+           primary.endpoint = ifelse(primary.endpoint == "YES", 1,0),
+           secondary.endpoint = ifelse(secondary.endpoint == "YES", 1, 0)) %>%
+    # Rename to make the code easier to understand
+    rename(primary.time = studydayprimarydate,
+           secondary.time = studydaysecondarydate)
 
-#Merging baseline and status of arbitration datasets to start building the outcomes dataset
-outcomes <- merge(baseline, status_arbitration, by = "a_subjectno", all = TRUE) %>%
-    #New variable called "time" where the time object for the Cox analysis will be stored for every subject
-    #In the status_arbitration dataset there is the studydayprimarydate, which is the date when the primary outcome, if it did not occur is NA
-    mutate(time = ifelse(!is.na(studydayprimarydate), studydayprimarydate, NA))
-
-#CHANGE OF STATUS
-#Dataset with information about status changes, such as early withdrawls, dead, lost to follow up...
-change_status <- read_csv("./demo_rct/trial_data/tbla_StatusChangeHistory.csv") %>%
-    basic_edit() %>%
-    col_class() %>%
-    #statuschangerowid is not found in any other table
-    subset(select=-c(statuschangehistoryrowid)) %>%
-    #Status_change variables show the time in which subjects were lost to follow up or abandoned the trial
-    #3 subjects have 2 changes of status, only interested in the last status change
-    group_by(a_subjectno) %>%
-    arrange(studydaystatuschangedate) %>%
-    slice(n())
-
-#Addition of change_status to outcome
-
-outcomes <- 
-    merge(outcomes, change_status, by = "a_subjectno", all = TRUE)%>%
-    #In the baseline dataset there was also a "status change date" variable
-    #Both columns relating to status don't show the same values for the subjects and have NA
-    #6 subjects without primary endpoint have different status change dates between those two columns 
-    mutate(
-    #New variable to join both status change date preferently keeping the date from the change_status dataset (after reviewing differences, it was assessed that it was more logical)
-        status_change = ifelse(!is.na(studydaystatuschangedate), studydaystatuschangedate, trialstatuschangedate),
-    #Updating time variable to add time for those subjects with status change
-        time = ifelse(!is.na(time), time, status_change)
-    )
-#In the time variable there is NA values for those that did not withdrawn or had the primary outcome
-#For these subjects, the time object will be defined as the day of the last visit
-
-#LAST VISIT
-##Importing the data
+# Importing dataset for the censored times
 last_visit <- read_csv("./demo_rct/trial_data/tbla_VisitSchedule.csv") %>%
     basic_edit() %>%
     col_class() %>%
-    #There are all the visits a subject has done throughout the trial (subjectno repeated for every visit)
+    # There are all the visits a subject has done throughout the trial (subjectno repeated for every visit)
     group_by(a_subjectno) %>%
-    #Only interested in the last visit 
+    # Only interested in the last visit 
     slice(which.max(studydayfromactualdate)) %>%
-    #change name of the variable and drop the other columns
+    # Change name of the variable and drop the other columns
     transmute(last_visit = studydayfromactualdate)
     
-#Addition to the outcomes dataset
-outcomes <- merge(outcomes, last_visit, by = "a_subjectno", all = TRUE) %>%
-    #Add last visit to the subjects without time object
-    mutate(time = ifelse(!is.na(time), time, last_visit)) %>%
-    #Some subjects have changes of status after the trial follow-up period (>3.5 years), change those to the last visit
-    mutate(time = ifelse(time > 3.5 * 365.25 & primary.endpoint == 0, last_visit, time))
+# Merging last two datasets into one
+outcomes <- merge(status_arbitration, last_visit, by = "a_subjectno", all = TRUE) %>%
+    # Add last visit time to the subjects without time object
+    mutate(primary.time = ifelse(!is.na(primary.time), primary.time, last_visit),
+           secondary.time = ifelse(!is.na(secondary.time), secondary.time, last_visit))
+
+# Importing dataset created with baseline.r
+baseline <- read.csv("./results/baseline_factors.csv") %>%
+    # Select only the variables important for the statistical analysis
+    select(c(a_subjectno, a_centreno, treatmentno, smoker, age, azathioprine, sixmp, thiopurines, surgery, age_diagnosis, infliximab_methotrexate, disease_duration))
+
+outcomes <- merge(baseline, outcomes, by = "a_subjectno", all = TRUE)
 
 
 #Write final csv
